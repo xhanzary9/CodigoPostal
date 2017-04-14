3d080cc6835ead63947e7c71cf46889a6b9fd9cf
Alejandro Anzures <alejandreyes36@gmail.com>
2017-04-14 16:59:32 -0500

    fabrica de Establecimientos

--- codigopostal/src/codigopostal/Contolador/FabricaEstableciminetos.java
+++ codigopostal/src/codigopostal/Contolador/FabricaEstableciminetos.java
@@ -5,7 +5,11 @@ import codigopostal.Modelo.Establecimiento;
 import codigopostal.Modelo.Restaurante;
 
 public class FabricaEstableciminetos {
-
+    /**
+     * Clase que crea una instancia de una clase que hereda a Establecimiento
+     * @param nombreEstablecimiento Linea de la base de datos que contiene el nombre del establecimiento
+     * @return clase que hereda a la clase establecimiento 
+     */
     public Establecimiento creaEstablecimiento(String nombreEstablecimiento) {
         if (nombreEstablecimiento.contains("Clinica Odontologia")) {
             return new Dentista();
